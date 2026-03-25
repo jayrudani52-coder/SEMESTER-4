72dea263edbe12af1902a5d34b6862e1a7a756e4
JAY RUDANI <jayrudani52@gmail.com>
2026-03-25 10:15:31 +0530

    Restore Node.js server code with JSON response
    Restored the server code and added a comment explaining functionality.

--- FSD/Chapter-1,2,3/fs13.js
+++ FSD/Chapter-1,2,3/fs13.js
@@ -1,3 +1,4 @@
+/*    Write a Node.js program using the HTTP module to create a server that sends a JSON response when the user visits the /api route.The JSON object should contain:name as "Rahul", course as "NodeJS",status as "Enrolled"Set the appropriate header for JSON data and display "404 – Page Not Found" for any other route.    */
 var http=require("http");
 var server=http.createServer((req, res) => {
     res.writeHead(200,{"Content-Type":"application/json"});
