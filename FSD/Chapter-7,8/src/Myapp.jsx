86c76c70ec818c94dca06248d8d21a7a32640889
JAY RUDANI <jayrudani52@gmail.com>
2026-06-05 13:02:59 +0530

    Add files via upload

---
+++ FSD/Chapter-7,8/src/Myapp.jsx
@@ -0,0 +1,21 @@
+// import Comp1 from "./Comp1.jsx";
+// import Task from "./Task.jsx";
+// import Map from "./Map.jsx";
+// import Parent from "./Parent.jsx";
+// import Productlist from "./Productlist.jsx";
+// import Form1 from "./Form1.jsx";
+// import UC1 from "./UC1.jsx";
+// import Const from "./Const.jsx";
+// import Task1 from "./Task1.jsx";
+//import UE1 from "./UE1.jsx";
+//import UE2 from "./UE2.jsx";
+//import A1 from "./A1.jsx";
+import Mypage from "./Mypage.jsx";
+function Myapp(){
+    return(
+        <>
+            <Mypage/>
+        </>
+    )
+}
+export default Myapp;
